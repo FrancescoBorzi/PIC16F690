b1346c21fb9ea89d8567e5c548c887e76b47eeb4
GigaDev90 <gigante.gabriele@hotmail.it>
2012-12-30 16:54:21 +0100

    Modificato il design
    adesso il converitore analogico non viene messo più sotto interrupt ma
    chiamato come funzione

--- autovelox.c
+++ autovelox.c
@@ -4,7 +4,7 @@
  *
  * Created on 22 dicembre 2012, 18.26
  */
-#define _XTAL_FREQ   4000000
+#define _XTAL_FREQ   8000000
 
 #include <xc.h>
 #include <pic16f690.h>
@@ -33,42 +33,55 @@ void putch(unsigned char byte) {
 int adc, us, ms, s, test, T_DELAY, adc1, adc2 = 0;
 
 void interrupt isr(void) {
+   // if (INTCONbits.T0IF == 1) {
+    us = us + 250;
+            if (us == 1000) {
+                us = 0;
+                ms++;
+                if(ms == 1000) {
+                    s++;
+                    ms = 0;
+                    PORTCbits.RC0 = !PORTCbits.RC0;
+                }
+                
+            }
+        TMR0 = T_DELAY;
+        INTCONbits.T0IF = 0;
+    //}
+        
+    
 
-    if (PIR1bits.ADIF == 1) {
-        PIR1bits.ADIF = 0;
-        ADCON0bits.GO_DONE = 1;
+//    if (PIR1bits.ADIF == 1) {
+//
+//        adc2 = adc1;
+//        adc1 = adc;
+//        adc = (ADRESH << 8) + ADRESL;
+//        PIR1bits.ADIF = 0;
+//        ADCON0bits.GO = 1;
+//    } 
+   
+    
+
+
+
+}
+void update() {
         adc2 = adc1;
         adc1 = adc;
-        adc = (ADRESH << 8) + ADRESL;
-        
-       
-        PORTCbits.RC1 = !PORTCbits.RC1;
-    }
-
-    if (INTCONbits.T0IF == 1) {
-        TMR0 = T_DELAY;
-        INTCONbits.T0IF = 0; 
-        if (test) {
-            ms++;
-            if (ms == 1000) {
-                ms = 30;
-                s++;
-                PORTCbits.RC0 = !PORTCbits.RC0;
-            }
-        }
-        
-    }
-PORTCbits.RC2 = !PORTCbits.RC2;
+        ADCON0bits.GO = 1; // start conversion
+        while (ADCON0bits.GO == 1); // wait for end of conversion
+            adc = (ADRESH << 8) + ADRESL;
+   
 }
 
 int main(void) {
     us = ms = s = test = 0;
     //frequenza settata a 4Mhz
-    OSCCONbits.IRCF = 0b110;
+    OSCCONbits.IRCF = 0b111;
     //registri per settare le porte da analogico a digitale, inizialmente le setto tutte a digitale
     ANSEL = 0; //e successivamente mi setto le porte analogiche che mi servono
     ANSELH = 0;
-    T_DELAY = 256 - 4;
+    T_DELAY = 256 - 250;
     /* configure digital I/O */
     TRISCbits.TRISC0 = 0;
     TRISCbits.TRISC1 = 0;
@@ -98,48 +111,51 @@ int main(void) {
 
     ADCON0bits.ADFM = 1; // right justified
     ADCON0bits.VCFG = 0; // Reference = VDD
-    ADCON0bits.CHS = 0b0010; // Select channel 0
-    // turn adc ok
-
-    ADCON1bits.ADCS = 0b110; // ADC clock = FOSC/64
-    INTCONbits.PEIE = 1; //enable peripheral interrupt ?
+    ADCON0bits.CHS = 0b0010; // Select channel 
+    ADCON0bits.ADON = 1; //enable adc module
+    ADCON1bits.ADCS = 0b000; // ADC clock =FOSC/2
+   // INTCONbits.PEIE = 1; //enable peripheral interrupt ?
     //abilito l'interrupt del converitore
-    PIR1bits.ADIF = 0;
-    PIE1bits.ADIE = 1;
+//    PIR1bits.ADIF = 0;
+//    PIE1bits.ADIE = 1;
 
-    ADCON0bits.ADON = 1;
-    ADCON0bits.GO = 1;
-
+    
+//setto il timer
     OPTION_REGbits.T0CS = 0;
-    OPTION_REGbits.PS = 0b111;
+    OPTION_REGbits.PS = 0b000;
     OPTION_REGbits.PSA = 0;
-
+//faccio partire il timer
     INTCONbits.T0IF = 0;
-    INTCONbits.T0IE = 1; // enable interrupt on timer 0
-
+   // INTCONbits.T0IE = 1; // enable interrupt on timer 0
+//abilito l'interrupt generale
     INTCONbits.GIE = 1;
 
 
 
     for (;;) {
-        adc = adc1 = adc2 = 0;
-        while ((adc + adc1 + adc2) / 3 < 200) ;
+        do {
+            update();    
+        }while((adc + adc1 + adc2) / 3 < 200);
         ADCON0bits.CHS = 0b1010;
-        test = 1;
+        INTCONbits.T0IE = 1;
         //printf("start\n\r");
         adc = adc1 = adc2 = 0;
-        while ((adc + adc1 + adc2) / 3 < 200)PORTCbits.RC3 = !PORTCbits.RC3;
-        test = 0;
+        
+        do {
+            update();
+        }while ((adc + adc1 + adc2) / 3 < 200);
+        INTCONbits.T0IE = 0;
         ADCON0bits.CHS = 0b0010;
+        adc = adc1 = adc2 = 0;
 
-        printf("%ds %dms\n\r", s, ms);
+        printf("%ds %dms %dus\n\r", s, ms, us);
 
-        ms = s = 0;
+        ms = s = us = 0;
         printf("\n\r");
         printf("\n\r");
         printf("\n\r");
         printf("\n\r");
-         __delay_ms(500);
+        __delay_ms(500);
 
 
     }
